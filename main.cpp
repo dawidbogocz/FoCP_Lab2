6cbfe07103b0af5f814bb5bbf7210518453e0d12
Dawid Bogocz <dawibog325@student.polsl.pl>
2020-10-15 12:25:22 +0200

    First function

--- main.cpp
+++ main.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 
-int main(){
-    // 1- Read a number from console
-    // 2- Figure out if the number is prime or not
-    // 3- Print the result to the console
+bool is_prime(int number){
+    for(int i=2 ; i<number ; i++){
+        if(number % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
 
+int main(){
     std::cout << "Please introduce a number" << std::endl;
     int number;
     std::cin >> number;
 
-    for(int i=2;i<number;i++){
-        if(number%i==0){
-            std::cout << "Your number is not prime" << std::endl;
-            return 0;
-        }
+    if(is_prime(number)){
+        std::cout << "Your number is prime" << std::endl;
+    }else{
+        std::cout << "Your number is not prime" << std::endl;
     }
-    std::cout << "Your number is prime" << std::endl;
+
+
     return 0;
 }
