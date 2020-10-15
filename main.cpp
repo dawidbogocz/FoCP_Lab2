d35b45218c3d5829286d56bbbebb477562cafb65
Dawid Bogocz <dawibog325@student.polsl.pl>
2020-10-15 12:59:43 +0200

    Fibonacci iterative

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
+int fibonacci_iterative(int index){
+    int a=0;
+    int b=1;
 
-int pow(int base, int power){
-    int result = 1;
-    for(int i=0; i<power; i++){
-        result *= base;
+    if(index==0 || index==1){
+        return index;
     }
-    return result;
+
+    for(int i=2; i<index; i++){
+        int tmp = a+b;
+        a=b;
+        b=tmp;
+    }
+    return b;
 }
 
 int read_int(){
@@ -18,10 +25,10 @@ int read_int(){
 }
 
 int main(){
-    int base = read_int();
-    int power = read_int();
+    int index = read_int();
 
-    int result = pow(base, power);
+
+    int result = fibonacci_iterative(index);
 
     std::cout << "The result is: " << result << std::endl;
 
