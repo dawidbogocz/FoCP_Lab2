f51e1669b6c94a0dcbb4d3cadb76513f09c8fb2b
Dawid Bogocz <dawibog325@student.polsl.pl>
2020-10-21 20:17:45 +0200

    Multiplication table to console (with comments and safety lock

--- multiplication_table.cpp
+++ multiplication_table.cpp
@@ -4,7 +4,8 @@
 #include <vector>
 #include <iomanip>
 
-int read_int(){
+int read_int() //function taking input from user
+{
     std::cout<<"Please introduce a number"<<std::endl;
     int n;
     std::cin>>n;
@@ -12,6 +13,11 @@ int read_int(){
     return n;
 }
 
+/**
+Function that creates multiplication table with number of rows and columns given by user.
+    @param I number of rows (default number is 5)
+    @param J number of columns (default number is 5)
+**/
 int create_table(int I=5, int J=5)
 {
     int s;
@@ -31,6 +37,12 @@ int main(){
     std::cout<<"How long should be your table?"<<std::endl;
     int tn=read_int();
 
+    if (tn==0)
+    {
+        std::cout<<"You've given 0 or a letter so the program will shut down";
+        return 0;
+    }
+
     int s;
     for (int i=1; i<=1; i++)
     {
@@ -45,6 +57,8 @@ int main(){
     std::cout<<"--+------------------------------------------"<<std::endl;
     create_table(tn, tn);
     std::cout<<"---------------------------------------------"<<std::endl;
+
+
 
     return 0;
 }
